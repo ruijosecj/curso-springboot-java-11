9a09e7f498307e027203b19f39d5ea74c5399290
Rui José <ruijosecj@gmail.com>
2020-08-04 21:09:16 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/ruijosecj/curso/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.ruijosecj.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ruijosecj.curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
---
+++ src/main/java/com/ruijosecj/curso/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.ruijosecj.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ruijosecj.curso.entities.Order;
+import com.ruijosecj.curso.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll(){
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj =  orderRepository.findById(id);
+		return obj.get();
+	}
+}
