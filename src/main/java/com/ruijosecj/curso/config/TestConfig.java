e29520a10eced2b4ae3f24ff4df444daacba76aa
Rui José <ruijosecj@gmail.com>
2020-08-06 10:50:43 -0300

    Payment, one-to-one association

--- src/main/java/com/ruijosecj/curso/config/TestConfig.java
+++ src/main/java/com/ruijosecj/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ruijosecj.curso.entities.Category;
 import com.ruijosecj.curso.entities.Order;
 import com.ruijosecj.curso.entities.OrderItem;
+import com.ruijosecj.curso.entities.Payment;
 import com.ruijosecj.curso.entities.Product;
 import com.ruijosecj.curso.entities.User;
 import com.ruijosecj.curso.entities.enuns.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
